33917cd5167618e7f071d5dfaee0bd2bee39eb6f
Pedro Henrique <pedrohenrique1603@gmail.com>
2023-10-25 17:13:42 -0300

    Finalizado

--- src/br/senai/sp/jandira/App.java
+++ src/br/senai/sp/jandira/App.java
@@ -12,16 +12,16 @@ public class App {
 
         System.out.println("Nome: " + gerente.getNome());
         System.out.println("ID: " + gerente.getId());
-        System.out.println("Salário Base: R$" + gerente.getBaseSalario());
-        System.out.println("Bônus de Gerência: R$" + gerente.getBonus());
+        System.out.println(" O salário base é de: " + gerente.getBaseSalario());
+        System.out.println("Bônus de Gerência é de: " + gerente.getBonus());
         System.out.println("Salário Total: R$" + gerente.calcularSalario());
         System.out.println();
 
         System.out.println("Informações do Programador:");
         System.out.println("Nome: " + programador.getNome());
         System.out.println("ID: " + programador.getId());
-        System.out.println("Salário Base: R$" + programador.getBaseSalario());
-        System.out.println("Salário Total: R$" + programador.calcularSalario());
+        System.out.println("O salário base é de " + programador.getBaseSalario());
+        System.out.println("O salário total é de: " + programador.calcularSalario());
     }
 
 
